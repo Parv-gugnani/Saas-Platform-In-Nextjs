231549e5ee0c59b340dfc711056459585dc695fc
Parv-gugnani <9765.parv@gmail.com>
2024-02-23 16:16:12 +0530

    branding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import MobileNav from "./MobileNav";
+import Image from "next/image";
 
 const Navbar = () => {
   //   const { getUser } = getKindeServerSession();
@@ -11,7 +12,10 @@ const Navbar = () => {
       <MaxWidthWrapper>
         <div className="flex h-14 items-center justify-between border-b border-zinc-200">
           <Link href="/" className="flex z-40 font-semibold">
-            <span>MomentumX.</span>
+            <span className="flex items-center">
+              <img src="/m.jpg" alt="m" className="w-6 h-6 mr-1" />
+              <span className="text-xl font-bold">MomentumX.</span>
+            </span>
           </Link>
 
           {/* <MobileNav isAuth={!!user} /> */}
