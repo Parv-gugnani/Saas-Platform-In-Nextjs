cf437194164a4863818d495fab280bca094deed9
Parv-gugnani <9765.parv@gmail.com>
2024-02-25 13:47:32 +0530

    uploadbut

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,11 +1,48 @@
 "use client";
 
+import { trpc } from "@/app/_trpc/client";
+import UploadButton from "./UploadButton";
+import { Ghost, Loader2, MessageSquare, Plus, Trash } from "lucide-react";
+import Skeleton from "react-loading-skeleton";
+import Link from "next/link";
+import { format } from "date-fns";
+import { Button } from "./ui/button";
+import { useState } from "react";
+import { getUserSubscriptionPlan } from "@/lib/stripe";
 interface PageProps {
   subscriptionPlan: Awaited<ReturnType<typeof getUserSubscriptionPlan>>;
 }
 
 const Dashboard = () => {
-  return <h1>This is Dashboard comp</h1>;
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+    string | null
+  >(null);
+
+  const utils = trpc.useContext();
+
+  const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+    onMutate({ id }) {
+      setCurrentlyDeletingFile(id);
+    },
+    onSettled() {
+      setCurrentlyDeletingFile(null);
+    },
+  });
+
+  return (
+    <main className="mx-auto max-w-7xl md:p-10">
+      <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
+        <h1 className="mb-3 font-bold text-5xl text-gray-900">My Files</h1>
+
+        <UploadButton isSubscribed={subscriptionPlan.isSubscribed} />
+      </div>
+    </main>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/UploadButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useState } from "react";
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import { Button } from "./ui/button";
+
+import Dropzone from "react-dropzone";
+import { Cloud, File, Loader2 } from "lucide-react";
+import { Progress } from "./ui/progress";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "./ui/use-toast";
+import { trpc } from "@/app/_trpc/client";
+import { useRouter } from "next/navigation";
+const UploadDropZone = ({ isSubscribed }: { isSubscribed: boolean }) => {
+  const router = useRouter();
+
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  const { toast } = useToast();
+
+  const { startUpload } = useUploadThing(
+    isSubscribed ? "proPlanUploader" : "freePlanUploader"
+  );
+
+  const { mutate: startPolling } = trpc.getFile.useMutation({
+    onSuccess: (file) => {
+      router.push(`/dashboard/${file.id}`);
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+};
