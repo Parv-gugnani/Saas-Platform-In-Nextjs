9e94857f78e1632bbbba9e4bd0600fbc1430759d
Parv-gugnani <9765.parv@gmail.com>
2024-03-14 23:18:03 +0530

    blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from "react";
 import { Ref } from "react";
 import { useScroll } from "framer-motion";
 import React from "react";
+import { Card } from "@/components/ui/scroll-anim";
+import { CardContent } from "@/components/ui/card";
 
 const BlogPage = () => {
   return (
@@ -11,6 +13,8 @@ const BlogPage = () => {
       <h1 className="text-4xl font-semibold tracking-tight lg:text-5xl text-center mt-5">
         Blogs
       </h1>
+      This is a blog BlogPage
+      <CardContent>This is my blog click to open</CardContent>
     </div>
   );
 };
